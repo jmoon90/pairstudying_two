34a13dc4ea60a1d86a326c5fb4c9e9d08d117ac0
John Moon <johnmoonyy@gmail.com>
2014-02-25 10:56:11 -0500

    Initial commit. Add gems in test and development mode

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PairStudying::Application.config.secret_key_base = '4ee05ab8820aa82cd5199ecc5f54793f3cd0662d11f1a887c9e0e83677d50cb63845a8acb811699d6c4fd491b4df8c632aae1351d49ed6e76f44ba22e67a9bb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PairStudying::Application.config.session_store :cookie_store, key: '_PairStudying_session'
